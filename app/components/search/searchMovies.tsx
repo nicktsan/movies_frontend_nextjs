3b193b9c699aaa89fe3607c368f48fe2afe04c40
nicktsan <nicktsang.manhay@gmail.com>
2023-10-20 19:37:06 -0700

    fixed a bug in searchMovies

--- app/components/search/searchMovies.tsx
+++ app/components/search/searchMovies.tsx
@@ -1,10 +1,16 @@
 'use client'
-import React, { useState } from 'react';
+import React, { ChangeEvent, useState } from 'react';
 import { useRouter } from 'next/navigation';
 function SearchMovies({ search }: { search?: string }) {
     const router = useRouter()
+    const [movieSearch, setMovieSearch] = useState(search);
+
     function GoToSearchParams(formData: FormData) {
         router.push(`/search-params?search=${formData.get('search')}`)
+    }
+
+    function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+        setMovieSearch(event.target.value);
     }
 
     return (
@@ -12,7 +18,7 @@ function SearchMovies({ search }: { search?: string }) {
             <form action={GoToSearchParams}>
                 <label>
                     Search for movies:
-                    <input defaultValue={search} className="border 4 border-black" type="text" name="search" />
+                    <input onChange={handleChange} value={movieSearch} className="border 4 border-black" type="text" name="search" />
                 </label>
                 <button className="py-1 px-2 bg-gray-100" type="submit">Search</button>
             </form>
