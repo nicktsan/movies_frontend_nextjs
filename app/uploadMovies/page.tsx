ef3172ee3d2b9ac0e550a7e12b9bd60920074d67
nicktsan <nicktsang.manhay@gmail.com>
2024-01-18 16:42:27 -0800

    Created a skeleton uploadMovies page that is only visible to admin role

--- app/uploadMovies/page.tsx
+++ app/uploadMovies/page.tsx
@@ -6,33 +6,25 @@ import MovieOwnershipList from "../components/search/MovieOwnershipList";
 import getAllMovieOwnership from "../utils/getAllMovieOwnership";
 import { SearchMyMovies } from "../components/search/searchMyMovies";
 //a protected can be navigated to with localhost:3000/mymovies if the user is signed in.
-export default async function MyMovies() {
+export default async function UploadMovies() {
     const session = await getServerSession();
     //if user is not signed in, redirect to sign in page
-    if (!session || !session.user) {
+    if (!session || !session.user.role?.find((element) => element === 'admin')) {
         // redirect("/api/auth/signin");
         return (
             <div>
                 <h1>My Movies Page</h1>
                 <br />
-                Please sign in to access this content.
+                You will only see this if you are an admin.
             </div>
         )
     }
-    // console.log("session: ", session)
-    const movies = await getAllMovieOwnership()
-    // console.log("movieOwnership:")
-    // console.log(movieOwnership)
-    //otherwise, user can access the mymovies page
-    //by default we want the movieownershiplist to display all movies the user owns
 
     return (
         <div>
-            <SearchMyMovies />
-            <h1>My Movies Page</h1>
+            <h1>Upload Movies Page</h1>
             <br />
-            You will only see this if you are authenticated.
-            <MovieOwnershipList movieOwnership={movies} />
+            Please be an admin to see this content
         </div>
     )
 }
