18b475d7c5fcf93b0a4e5f01a337786693de96d4
nicktsan <nicktsang.manhay@gmail.com>
2024-01-29 18:03:46 -0800

    Added embedded mux video player

--- app/components/watch/WatchButton.tsx
+++ app/components/watch/WatchButton.tsx
@@ -1,10 +1,11 @@
-'use client'
+"use client"
 import { signIn, useSession } from "next-auth/react"
 import { MovieOwnership } from '../movies/MovieOwnership'
-
+import getMuxVideo from "@/app/utils/getMuxVideo";
+import { useRouter } from 'next/navigation';
 export default function WatchButton({ movieOwnership }: { movieOwnership: MovieOwnership }) {
     const { data: session } = useSession();
-
+    const router = useRouter();
     //if user is not signed in, redirect them to signin page. If they are signed in, redirect them to checkout page
     function handleClick(event: React.MouseEvent) {
         event.preventDefault()
@@ -17,7 +18,11 @@ export default function WatchButton({ movieOwnership }: { movieOwnership: MovieO
 
     return (
         <>
-            <button type="button" id="watch" onClick={handleClick} className="py-1 px-2 bg-blue-100">Watch</button>
+            <button type="button" id="watch" onClick={async () => {
+                console.log("movieOwnership.title: ", movieOwnership.title)
+                const muxVideoData = await getMuxVideo(movieOwnership.title)
+                router.replace((`/watch/${muxVideoData[0].playbackid}`))
+            }} className="py-1 px-2 bg-blue-100">Watch</button>
         </>
     )
 }
